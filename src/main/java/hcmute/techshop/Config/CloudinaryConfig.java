4936594ab29c679cf2ce23525157635e9ea9aee8
nauht1 <thuanngo454@gmail.com>
2025-03-03 00:48:20 +0700

    add cloudinary upload image file service

--- src/main/java/hcmute/techshop/Config/CloudinaryConfig.java
+++ src/main/java/hcmute/techshop/Config/CloudinaryConfig.java
@@ -1,4 +1,18 @@
 package hcmute.techshop.Config;
 
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CloudinaryConfig {
+
+    @Value("${CLOUDINARY_URL}")
+    private String cloudinaryUrl;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(cloudinaryUrl);
+    }
 }

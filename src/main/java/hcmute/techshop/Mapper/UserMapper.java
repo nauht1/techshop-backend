b1aa2e3a8fd378d66bd2c2f0792f1a8c9c99e624
NNQA <anhla12h@gmail.com>
2025-03-27 17:09:41 +0700

    feat(user): add Mapper for user

--- src/main/java/hcmute/techshop/Mapper/UserMapper.java
+++ src/main/java/hcmute/techshop/Mapper/UserMapper.java
@@ -1,2 +1,14 @@
-package hcmute.techshop.Mapper;public class UserMapper {
+package hcmute.techshop.Mapper;
+
+import hcmute.techshop.Entity.Auth.UserEntity;
+import hcmute.techshop.Model.User.ProfileRequest;
+import org.mapstruct.*;
+
+@Mapper(
+        componentModel = "spring", // Integrates with Spring if you're using it
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE // Skip null fields
+)
+public interface UserMapper {
+    // Update existing UserEntity with data from another UserEntity
+    void updateUserFromUser(@MappingTarget UserEntity target, ProfileRequest source);
 }
